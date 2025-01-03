4f9ed327e100d361a23b97e02312172db59cb11a
Geethanjali918 <geethaguvvala8919@gmail.com>
2025-01-03 22:58:51 +0530

    Add files via upload
    username and pwd using while loop

---
+++ Demo6.java
@@ -0,0 +1,10 @@
+public class Demo6{
+    public static void main(String[] args){
+        String username="geetha_001";
+        String pwd="geetha_001";
+        while((username=="geetha_001")&&(pwd=="geetha_001")){
+            System.out.println("hello world");
+            break;
+        }
+    }
+}
