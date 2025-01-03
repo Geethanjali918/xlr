9949db354a3fb27697518ba653e39ee5786a4478
Geethanjali918 <geethaguvvala8919@gmail.com>
2025-01-03 10:43:26 +0530

    Add files via upload
    while(user name and password)

---
+++ demo3.java
@@ -0,0 +1,10 @@
+public class Demo003{
+    public static void main(String[] args) {
+        String username = "user001";
+        String pwd = "user_001";
+        while((username == "user==001") && (pwd == user == "user 001")) {
+            System.out.println("hello user happy new year");
+            break;
+        }
+    }
+}
