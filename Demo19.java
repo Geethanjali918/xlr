a22d6c774c5302b5b0338b8fd3f5b0d6ea85d8b6
Geethanjali918 <geethaguvvala8919@gmail.com>
2025-01-09 12:48:22 +0530

    Add files via upload
    fibonacci series

--- Demo19.java
+++ Demo19.java
@@ -1,11 +1,32 @@
-import java.util.Scanner;
 public class Demo19 {
+    public static int[] getFibonacciSeries(int n) {
+        if (n <= 0) {
+            System.out.println(0);
+            return new int[]{};
+        }
+        int[] fibonacciSeries = new int[n];
+        fibonacciSeries[0] = 0;
+        if (n > 1) {
+            fibonacciSeries[1] = 1;
+        }
+        for (int i = 2; i < n; i++) {
+            fibonacciSeries[i] = fibonacciSeries[i - 1] + fibonacciSeries[i - 2];
+        }
+
+        return fibonacciSeries;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        int nextMultipleOfFive = (number / 5 + 1) * 5;
-        System.out.println("The next multiple of 5 is: " + nextMultipleOfFive);
-        scanner.close();
+        int n = 7;
+        int[] result = getFibonacciSeries(n);
+        if (result.length > 0) {
+            for (int i = 0; i < result.length; i++) {
+                System.out.print(result[i]);
+                if (i < result.length - 1) {
+                    System.out.print(", ");
+                }
+            }
+        }
     }
 }
+
