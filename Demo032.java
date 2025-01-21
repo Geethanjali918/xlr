17e2d13b2d0be7c2997ad10cd832609ff92e5304
Geethanjali918 <geethaguvvala8919@gmail.com>
2025-01-21 12:26:31 +0530

    homogenous string

---
+++ Demo032.java
@@ -0,0 +1,11 @@
+import java.util.HashSet;
+
+public class Demo032 {
+    public static void main(String[] args) {
+        HashSet<String> stringSet = new HashSet<>();
+        stringSet.add("Hello");
+        stringSet.add("World");
+        stringSet.add("Java");
+        System.out.println(stringSet);
+    }
+}
