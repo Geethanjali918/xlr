f261cd223a80496eeb98675c22ae26415eed0760
Geethanjali918 <geethaguvvala8919@gmail.com>
2025-01-25 12:22:28 +0530

    checking big number between two numbers

---
+++ Demo035.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Demo035 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        if (num1 > num2) {
+            System.out.println(num1 + " is larger than " + num2);
+        } else if (num2 > num1) {
+            System.out.println(num2 + " is larger than " + num1);
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+        scanner.close();
+    }
+}
